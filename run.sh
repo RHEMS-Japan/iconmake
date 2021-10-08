6e167a75ce0c9c8a492218016893fb2827b4f294
miwakooe <miwakooe@rhems-japan.co.jp>
2021-10-08 18:23:05 +0900

    fix

--- run.sh
+++ run.sh
@@ -6,29 +6,55 @@ _tag_name=${TAG}
 _imgname=${IMG}
 _org_image=${_tmp_dir}/${_imgname}
 _corner=corner.png
-_cornername=${CRN}
+_bar=bar.png
+_sealname=${SLN}
 
 
-## create tmp dir
-mkdir -p ${_tmp_dir}
 
-echo "## create corner"
-convert -pointsize 15 -gravity East -font ./omaner.ttf -fill white -annotate 48x45+7x25 "${_tag_name}" ${_cornername} ${_corner}
-#convert -pointsize 15 -gravity East -font ./omaner.ttf -annotate 48x45+7x25 "PRD" red-corner.png ${_corner}
 
-#echo "## annotare corner"
-#convert -fill white -annotate 48x45+7x25 "${_tag_name}" ${_cornername} ${_corner}
 
-echo "## change org image to fit"
-convert -resize 119x119! ${_org_image} ${_imgname}-fit.png
+if [ ${SRN} = "rhems-bar.png" ]; then
+    ## create tmp dir
+    mkdir -p ${_tmp_dir}
 
-echo "## change to 128x128"
-convert ${_imgname}-fit.png -gravity center -extent 128x128 ${_imgname}-resize.png
+    echo "## create bar"
+    convert -pointsize 15 -gravity East -font ./omaner.ttf  rhems-bar.png corner.png
 
-echo "## create frame(red #ff0000)"
-convert ${_imgname}-resize.png -shave 3x3 -bordercolor "#ff0000" -border 3 ${_imgname}-frame.png
+    echo "## change org image to fit"
+    convert -resize 119x119! ${_org_image} ${_imgname}-fit.png
 
-echo "## add corner"
-convert ${_imgname}-frame.png ${_corner} -gravity  NorthEast -compose over -composite `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png
+    echo "## change to 128x128"
+    convert ${_imgname}-fit.png -gravity center -extent 128x128 ${_imgname}-resize.png
 
-mv -i `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png ${_tmp_dir}
+    echo "## create frame(red #ff0000)"
+    convert ${_imgname}-resize.png -shave 3x3 -bordercolor "#ff0000" -border 3 ${_imgname}-frame.png
+
+    echo "## add bar"
+    convert ${_imgname}-frame.png ${_bar} -gravity  South -compose over -composite `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png
+
+    mv -i `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png ${_tmp_dir}
+
+
+else
+    ## create tmp dir
+    mkdir -p ${_tmp_dir}
+
+    echo "## create corner"
+    convert -pointsize 15 -gravity East -font ./omaner.ttf -fill white -annotate 48x45+7x25 "${_tag_name}" ${_cornername} ${_corner}
+    #convert -pointsize 15 -gravity East -font ./omaner.ttf -annotate 48x45+7x25 "PRD" red-corner.png ${_corner}
+
+    echo "## change org image to fit"
+    convert -resize 119x119! ${_org_image} ${_imgname}-fit.png
+
+    echo "## change to 128x128"
+    convert ${_imgname}-fit.png -gravity center -extent 128x128 ${_imgname}-resize.png
+
+    echo "## create frame(red #ff0000)"
+    convert ${_imgname}-resize.png -shave 3x3 -bordercolor "#ff0000" -border 3 ${_imgname}-frame.png
+
+    echo "## add corner"
+    convert ${_imgname}-frame.png ${_corner} -gravity  NorthEast -compose over -composite `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png
+
+    mv -i `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png ${_tmp_dir}
+
+fi
