27e4e5beb4144bc0ed7fdfad3d4337a085073f38
miwakooe <miwakooe@rhems-japan.co.jp>
2021-10-11 13:41:54 +0900

    fix

--- run.sh
+++ run.sh
@@ -24,13 +24,13 @@ if [ ${_sealname} = "rhems-bar.png" ]; then
     convert ${_bar} -resize 128x128 ${_bar}-resize.png
 
     echo "## change org image to fit"
-    convert -resize 119x119! ${_org_image} ${_imgname}-fit.png
+    convert -resize 105x105! ${_org_image} ${_imgname}-fit.png
 
     echo "## change to 128x128"
-    convert ${_imgname}-fit.png -gravity center -extent 128x128 ${_imgname}-resize.png
+    convert ${_imgname}-fit.png -gravity North -extent 128x128 ${_imgname}-resize.png
 
     echo "## create frame(red #ff0000)"
-    convert ${_imgname}-resize.png -shave 3x3 -bordercolor "#ff0000" -border 3 ${_imgname}-frame.png
+    convert ${_imgname}-resize.png -shave 3x3 -bordercolor "#ff0000" -border 1 ${_imgname}-frame.png
 
     echo "## add bar"
     convert ${_imgname}-frame.png ${_bar}-resize.png -gravity  South -compose over -composite `echo ${_imgname} | cut -d '.' -f 1`-iconmaker.png
